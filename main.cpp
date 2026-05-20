3199acafb7fa70fafe7c84d4db60a5421fdfa7a1
synapse-dot <felixx.a.wayne32@gmail.com>
2026-05-20 13:40:02 +0530

    Updated logic to use variables to read current position and update next position

--- main.cpp
+++ main.cpp
@@ -5,8 +5,7 @@
 
 int main() {
   // Set origin Co-ordinates
-  std::vector<int> xPos;
-  std::vector<int> yPos;
+  std::vector<int> xPos, yPos;
 
   xPos.push_back(0);
   yPos.push_back(0);
@@ -20,32 +19,28 @@ int main() {
   std::cout << "How many steps to take: ";
   std::cin >> num_steps;
 
+  int x, y;
+
   for (int i = 0; i < num_steps; ++i) {
     /*
     Get a random direction 
     0 = up, 1 = right, 2 = down, 3 = left
     */
     direction = std::rand() % 4;
+
+    // Update xPos and yPos accordingly
+    x = xPos.back();
+    y = yPos.back();
     switch (direction) {
-      case 0:
-        xPos.push_back(xPos[i]);
-        yPos.push_back(yPos[i]+1);
-        break;
-      case 1:
-        xPos.push_back(xPos[i]+1);
-        yPos.push_back(yPos[i]);
-        break;
-      case 2:
-        xPos.push_back(xPos[i]);
-        yPos.push_back(yPos[i]-1);
-        break;
-      case 3:
-        xPos.push_back(xPos[i]-1);
-        yPos.push_back(yPos[i]);
-        break;
-      default:
-        std::cout << "Error";
+      case 0: y++; break;
+      case 1: x++; break;
+      case 2: y--; break;
+      case 3: x--; break;
+      default: std::cout << "Error";
     };
+
+    xPos.push_back(x);
+    yPos.push_back(y);
   };
 
   for (std::size_t i = 0; i < xPos.size(); ++i) {
